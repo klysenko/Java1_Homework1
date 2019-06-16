66664134fc2da979d3ba942d9eb3785ac8b91952
Kate Lysenko <lisenko.katya@gmail.com>
2019-06-16 11:50:06 +0300

    init commit

---
+++ web/src/main/java/CheckstyleChecker.java
@@ -0,0 +1,11 @@
+public class CheckstyleChecker {
+    public static void main(String[] args) {
+        sayHiTimes(10);
+    }
+
+    private static void sayHiTimes(int count) {
+        for (int i = 0; i <= count; i++) {
+            System.out.println("Hi!");
+        }
+    }
+}
